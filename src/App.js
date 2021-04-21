0c44d59b8574a0f0572f3bb008526fc2c39f8a66
Krushay Bhavsar <krushaybhavsar@gmail.com>
2021-04-20 21:41:49 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Education from "./pages/Education/Education";
+import Projects from "./pages/Projects/Projects";
+import Skills from "./pages/Skills/Skills";
+import Contact from "./pages/Contact/Contact";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/education" exact>
+            <Education />
+          </Route>
+          <Route path="/projects" exact>
+            <Projects />
+          </Route>
+          <Route path="/skills" exact>
+            <Skills />
+          </Route>
+          <Route path="/contact" exact>
+            <Contact />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Education/Education.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center" style={{ paddingTop: "30%" }}>
+        Education
+      </h1>
+    </div>
+  );
+};
+
+export default Education;
