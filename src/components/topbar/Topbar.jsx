430c3f23295389c9e84845f19c2342f295e96947
Krushay Bhavsar <krushaybhavsar@gmail.com>
2021-06-10 23:09:58 -0400

    Added Social Links

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,6 +3,9 @@ import "./topbar.scss";
 import { Person, Mail } from "@material-ui/icons";
 import "../../sections/projects/Projects.jsx";
 
+// https://uicookies.com/html-social-media-icons/
+// https://codepen.io/michaelrossi/pen/NPRzwO
+
 export default function Navbar({
   menuOpen,
   setMenuOpen,
@@ -17,6 +20,10 @@ export default function Navbar({
   const hamburgerActions = () => {
     setMenuOpen(!menuOpen);
     setModalState(false);
+  };
+
+  const redirectToLink = (linkURL) => {
+    window.open(linkURL, "_blank");
   };
 
   return (
@@ -40,6 +47,44 @@ export default function Navbar({
           </div>
         </div>
         <div className="right">
+          <div className="socialsContainer">
+            <div className="socials">
+              <div
+                className={"linkedInDiv " + (menuOpen && "active")}
+                onClick={() =>
+                  redirectToLink("https://www.linkedin.com/in/krushaybhavsar/")
+                }
+              >
+                <i className="fa fa-linkedin"></i>
+              </div>
+              <div
+                className={"githubDiv " + (menuOpen && "active")}
+                onClick={() =>
+                  redirectToLink("https://github.com/krushaybhavsar")
+                }
+              >
+                <i className="fa fa-github"></i>
+              </div>
+              <div
+                className={"instaDiv " + (menuOpen && "active")}
+                onClick={() =>
+                  redirectToLink("https://www.instagram.com/krushaybhavsar/")
+                }
+              >
+                <i className="fa fa-instagram"></i>
+              </div>
+              <div
+                className={"youtubeDiv " + (menuOpen && "active")}
+                onClick={() =>
+                  redirectToLink(
+                    "https://www.youtube.com/channel/UCgpfQDfxE8OgeKkBc9RTwXA/featured"
+                  )
+                }
+              >
+                <i className="fa fa-youtube"></i>
+              </div>
+            </div>
+          </div>
           <div className="hamburger" onClick={() => hamburgerActions()}>
             <span className="line1"></span>
             <span className="line2"></span>
