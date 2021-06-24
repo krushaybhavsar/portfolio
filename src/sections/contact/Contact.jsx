afdf1ddd14b8411c163666a4aa6ceb6d0bb4d1eb
Krushay Bhavsar <krushaybhavsar@gmail.com>
2021-06-23 22:41:57 -0400

    Fixed ReCAPTCHA

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -115,11 +115,6 @@ class Contact extends Component {
                 ></textarea>
               </p>
               <div class="recaptchaContainer">
-                <script
-                  src="https://www.google.com/recaptcha/api.js"
-                  async
-                  defer
-                ></script>
                 <Recaptcha
                   // sitekey={process.env.REACT_APP_LOCALHOST_RECAPTCHA}
                   sitekey={process.env.REACT_APP_NETLIFY_RECAPTCHA}
