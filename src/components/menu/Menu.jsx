4f24ab9b2c752a404d0d25be96277633c83f2199
Krushay Bhavsar <krushaybhavsar@gmail.com>
2021-06-25 09:18:51 -0400

    Added Mobile Social Handles

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -35,6 +35,43 @@ export default function Menu({ menuOpen, setMenuOpen }) {
           <a href="#contact">Contact</a>
         </li>
       </ul>
+
+      <div className="socialsContainer">
+        <div className="socials">
+          <div
+            className={"linkedInDiv " + (menuOpen && "active")}
+            onClick={() =>
+              redirectToLink("https://www.linkedin.com/in/krushaybhavsar/")
+            }
+          >
+            <i className="fa fa-linkedin"></i>
+          </div>
+          <div
+            className={"githubDiv " + (menuOpen && "active")}
+            onClick={() => redirectToLink("https://github.com/krushaybhavsar")}
+          >
+            <i className="fa fa-github"></i>
+          </div>
+          <div
+            className={"instaDiv " + (menuOpen && "active")}
+            onClick={() =>
+              redirectToLink("https://www.instagram.com/krushaybhavsar/")
+            }
+          >
+            <i className="fa fa-instagram"></i>
+          </div>
+          <div
+            className={"youtubeDiv " + (menuOpen && "active")}
+            onClick={() =>
+              redirectToLink(
+                "https://www.youtube.com/channel/UCgpfQDfxE8OgeKkBc9RTwXA/featured"
+              )
+            }
+          >
+            <i className="fa fa-youtube"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
