1617762c627b078f293458e175d83e6e90e210cd
Krushay Bhavsar <krushaybhavsar@gmail.com>
2025-10-13 22:13:06 -0400

    Updated Experience

--- src/data.ts
+++ src/data.ts
@@ -80,13 +80,17 @@ export const ExperienceSectionData: TimelineElementExperience[] = [
   },
   {
     id: 6,
-    jobPosition: "Backend Software Engineer Intern",
+    jobPosition: "Backend Software Engineer Intern | Trading Infrastructure",
     companyName: "Wealthfront",
     companyLogo: require("../src/assets/wealthfront-logo.png"),
     location: "Palo Alto, CA",
     date: "May 2025 – Aug 2025",
-    description: ["Incoming Summer 2025 – Trading Team"],
-    skillsUsed: ["Java", "JUnit"],
+    description: [
+      "Designed and implemented a low-latency trade order pricing system for low-liquidity, wide bid/ask spread instruments to improve allocation prices trades and reduce order cancellations using Java, Hibernate, and MariaDB",
+      "Authored and presented in-depth technical design documents with historical incident analysis, UML diagrams, and code-level insights to the VP of Engineering & senior leadership to deploy the optimized pricing system across trading infrastructure",
+      "Automated the pruning of stale trade account data via batch processing jobs on Apache Airflow, removing over 600K rows daily and 40 million rows of data total to improve query performance and ensure high concurrency for account processes",
+    ],
+    skillsUsed: ["Java", "JUnit", "MariaDB", "Hibernate ORM", "Apache Airflow", "Guice", "Guava"],
     links: [],
   },
 ];
